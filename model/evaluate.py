cbab69403b7ba63c18c2810dcb37629a11b06b3c
Tushar Arora <tushararora1235@gmail.com>
2025-08-19 11:09:03 +0530

    Initial Commit

---
+++ model/evaluate.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split 
+from sklearn.ensemble import accuracy_score
+import joblib
+
+
+#Load the dataset
+data=pd.read_csv('data/iris.csv')
+
+
+#Preprocess the dataset
+X=data.drop('species', axis=1)
+y=data['species']
+
+#Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=42)
+
+
+
+#load the Save model
+model = joblib.dump(model, 'model/iris_model.pkl')
+
+
+#make predictions 
+y_pred = model.predict(X_test)
+
+
+#evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Model accuracy: {accuracy:.2f}')
