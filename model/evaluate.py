8b682202a79f976ffe84ab61d79abf2f339b0ee1
Tushar Arora <tushararora1235@gmail.com>
2025-08-26 09:40:05 +0530

    Initial Commit 1.2

--- model/evaluate.py
+++ model/evaluate.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split 
-from sklearn.ensemble import accuracy_score
+from sklearn.metrics import accuracy_score
 import joblib
 
 
